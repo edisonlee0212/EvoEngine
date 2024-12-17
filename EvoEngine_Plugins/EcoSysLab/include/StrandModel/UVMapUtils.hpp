683d561a0358ade48db885c5018e8c1b2ac6d360
Nikolas Schwarz <38488786+nikolasschwarz@users.noreply.github.com>
2024-12-17 15:23:10 +0100

    fix crash caused by computing UV coordinates for experiments (#70)

--- EvoEngine_Plugins/EcoSysLab/include/StrandModel/UVMapUtils.hpp
+++ EvoEngine_Plugins/EcoSysLab/include/StrandModel/UVMapUtils.hpp
@@ -16,16 +16,16 @@ class UVMapUtils {
 
   static float GetPipePolar(const StrandModel& strand_model, const StrandHandle& pipe_handle, float t);
 
-  static const Particle2D<CellParticlePhysicsData>& GetEndParticle(const StrandModel& strand_model,
+  static const Particle2D<CellParticlePhysicsData>* GetEndParticle(const StrandModel& strand_model,
                                                                    const StrandHandle& pipe_handle, size_t index);
 
-  static const Particle2D<CellParticlePhysicsData>& GetEndParticle(const StrandModelSkeleton& skeleton,
+  static const Particle2D<CellParticlePhysicsData>* GetEndParticle(const StrandModelSkeleton& skeleton,
                                                             const StrandHandle& pipe_handle, size_t index);
 
-  static const Particle2D<CellParticlePhysicsData>& GetStartParticle(const StrandModel& strand_model,
+  static const Particle2D<CellParticlePhysicsData>* GetStartParticle(const StrandModel& strand_model,
                                                                      const StrandHandle& pipe_handle, size_t index);
 
-  static const Particle2D<CellParticlePhysicsData>& GetStartParticle(const StrandModelSkeleton& skeleton,
+  static const Particle2D<CellParticlePhysicsData>* GetStartParticle(const StrandModelSkeleton& skeleton,
                                                               const StrandHandle& pipe_handle, size_t index);
 
 };
