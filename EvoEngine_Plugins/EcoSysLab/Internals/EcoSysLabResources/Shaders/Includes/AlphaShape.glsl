5c822ec9abca2840dcad841137b59d783fb55181
Nikolas Schwarz <38488786+nikolasschwarz@users.noreply.github.com>
2025-01-20 10:44:39 +0100

    Add in-keyword to several functions (#188)

--- EvoEngine_Plugins/EcoSysLab/Internals/EcoSysLabResources/Shaders/Includes/AlphaShape.glsl
+++ EvoEngine_Plugins/EcoSysLab/Internals/EcoSysLabResources/Shaders/Includes/AlphaShape.glsl
@@ -119,11 +119,11 @@ float LongestSide(uint indices[4]) {
   return d;
 }*/
 
-bool InSameGroup(UniformParticle p0, UniformParticle p1) {
+bool InSameGroup(in UniformParticle p0, in UniformParticle p1) {
   return segments[p0.segment_handle].group_index == segments[p1.segment_handle].group_index;
 }
 
-bool RemainInSameGroup(DelaunayTetrahedron tet) {
+bool RemainInSameGroup(in DelaunayTetrahedron tet) {
   UniformParticle p[4];
   [[unroll]]
   for (uint i = 0; i < 4; i++) {
@@ -235,7 +235,7 @@ bool AreNeighbors(uint index0, uint index1)
 
 }
 
-float SkeletonStructure(DelaunayTetrahedron tet) {
+float SkeletonStructure(in DelaunayTetrahedron tet) {
 
   float d = 0.0;
 
@@ -252,7 +252,7 @@ float SkeletonStructure(DelaunayTetrahedron tet) {
   return d;
 }
 
-bool InsideAlpha(DelaunayTetrahedron tet, out float d) {
+bool InsideAlpha(in DelaunayTetrahedron tet, out float d) {
 
   //d = LongestSide(indices);
   d = SkeletonStructure(tet);
@@ -269,7 +269,7 @@ uvec3 triangles[] = {
     uvec3(9, 10, 11),
 };
 
-vec3 ComputeTriangleNormal(vec3 v0, vec3 v1, vec3 v2) {
+vec3 ComputeTriangleNormal(in vec3 v0, in vec3 v1, in vec3 v2) {
   // Compute the two edges of the triangle
   vec3 edge1 = v1 - v0;
   vec3 edge2 = v2 - v0;
@@ -281,7 +281,7 @@ vec3 ComputeTriangleNormal(vec3 v0, vec3 v1, vec3 v2) {
   return normalize(normal);
 }
 
-vec3 ComputeTetTriangleNormal(DelaunayTetrahedron tet, uint triangle_index) {
+vec3 ComputeTetTriangleNormal(in DelaunayTetrahedron tet, uint triangle_index) {
 
   uvec3 triangle = triangles[triangle_index];
   return ComputeTriangleNormal(uniform_particles[tet.indices[lookup[triangle[0]]]].position_t.xyz,
@@ -289,7 +289,7 @@ vec3 ComputeTetTriangleNormal(DelaunayTetrahedron tet, uint triangle_index) {
                                uniform_particles[tet.indices[lookup[triangle[2]]]].position_t.xyz);
 }
 
-bool IsDegenerateTriangle(vec3 v0, vec3 v1, vec3 v2, float epsilon) {
+bool IsDegenerateTriangle(in vec3 v0, in vec3 v1, in vec3 v2, float epsilon) {
   // Compute the edges of the triangle
   vec3 edge1 = v1 - v0;
   vec3 edge2 = v2 - v0;
