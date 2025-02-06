761096f4e6c827d38cf93aaba3e1a689f034ea8c
DemoySegment <48055300+DemoySegment@users.noreply.github.com>
2025-02-06 00:16:43 -0500

    Reconstruction of sorghum from prediction (#225)

--- EvoEngine_Plugins/DigitalAgriculture/include/SorghumModel/SorghumDescriptor.hpp
+++ EvoEngine_Plugins/DigitalAgriculture/include/SorghumModel/SorghumDescriptor.hpp
@@ -62,7 +62,9 @@ class SorghumDescriptor : public IAsset {
   void Deserialize(const YAML::Node& in) override;
   [[maybe_unused]] Entity CreateEntity(const std::string& name) const;
   [[nodiscard]] std::shared_ptr<Texture2D> GenerateThumbnailTexture() override;
-  void ImportPrediction(const std::filesystem::path& yaml_path);
+
+  [[nodiscard]] std::optional<std::vector<std::unordered_map<std::string, std::vector<glm::vec3>>>> ImportPrediction(
+      const std::filesystem::path& yaml_path);
 };
 
 }  // namespace digital_agriculture_plugin
